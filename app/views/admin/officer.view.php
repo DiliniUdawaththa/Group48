92c01dcfb211e43109b95060df8ae5fbb8633c24
DiliniUdawaththa <dmadhushani611@gmail.com>
2023-11-01 00:06:13 +0530

    admin.officer.view.php initially addeed

---
+++ app/views/admin/officer.view.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?=ucfirst(App::$page)?> - <?=APPNAME?></title>
+    <script src="https://kit.fontawesome.com/cbd2a66f05.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/Admin/Dashboard.css">
+</head>
+<body>
+   <?php $this->view('admin/include/sidebar',$data) ?>
+</body>
