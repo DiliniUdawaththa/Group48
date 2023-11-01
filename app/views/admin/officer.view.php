84c374731faf5ed3e2fc3a23c5bbaaa60b043f68
DiliniUdawaththa <dmadhushani611@gmail.com>
2023-11-01 10:22:53 +0530

    Dashboard.css and officer.view.php changed

--- app/views/admin/officer.view.php
+++ app/views/admin/officer.view.php
@@ -9,4 +9,61 @@
 </head>
 <body>
    <?php $this->view('admin/include/sidebar',$data) ?>
+   <section id="interface">
+        <div class="navi">
+            <div class="navi1">
+                <h2>OFFICER</h2>
+            </div>
+
+            <div class="operation">
+                <button type="button" class="button-style">+ Add Officer</button>
+            </div>
+        </div>
+
+        <div class="table1">
+            <table>
+                <thead>
+                    <tr>
+                        <td>empID</td>
+                        <td>Name</td>
+                        <td>Email</td>
+                        <td>Mobile</td>
+                        <td>Option</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>001</td>
+                        <td>Amila Perera</td>
+                        <td>amila@gmail.com</td>
+                        <td>0771234567</td>
+                        <td>
+                            <button class="update">Update</button>
+                            <button class="delete">Delete</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>002</td>
+                        <td>Nuwan Perera</td>
+                        <td>nuwan@gmail.com</td>
+                        <td>0771234567</td>
+                        <td>
+                            <button class="update">Update</button>
+                            <button class="delete">Delete</button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>003</td>
+                        <td>Dinithi Fernando</td>
+                        <td>dinithi@gmail.com</td>
+                        <td>0771234567</td>
+                        <td>
+                            <button class="update">Update</button>
+                            <button class="delete">Delete</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </section>
 </body>
