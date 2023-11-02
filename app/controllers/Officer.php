30698e34a84fd933b175bf1f1ec3e37b4de05ad8
Isuka Premathilake <isuka.premathilake@gmail.com>
2023-11-03 02:54:13 +0530

    Officer

--- app/controllers/Officer.php
+++ app/controllers/Officer.php
@@ -9,6 +9,33 @@
         $data['title'] = "Officer";
         $this->view('Officer/dashboard',$data);
     }
+    public function officerdriverRegistration(){
+        if(!Auth::logged_in())
+        {
+            message('please login to view the page');
+            redirect("login");
+        }
+        $data['title'] = "Officer";
+        $this->view('Officer/officerdriverRegistration',$data);
+    }
+    public function complains(){
+        if(!Auth::logged_in())
+        {
+            message('please login to view the page');
+            redirect("login");
+        }
+        $data['title'] = "Officer";
+        $this->view('Officer/complains',$data);
+    }
+    public function standardFare(){
+        if(!Auth::logged_in())
+        {
+            message('please login to view the page');
+            redirect("login");
+        }
+        $data['title'] = "Officer";
+        $this->view('Officer/standardFare',$data);
+    }
  }
  //echo " sample home page";
  ?>
