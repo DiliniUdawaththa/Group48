337907185f8c7c20e1d0bc2980413b73ac06d2c9
Thusikaran <pushparasa.thusikaran@gmail.com>
2024-03-23 17:01:14 +0530

    resolve commit

--- app/views/customer/ride_step5.view.php
+++ app/views/customer/ride_step5.view.php
@@ -120,7 +120,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = ""; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
