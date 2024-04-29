8df26e2c3da0d722f753ab8c8060f84e2ba55f31
Thusikaran <pushparasa.thusikaran@gmail.com>
2024-04-29 19:40:29 +0530

    Changes

---
+++ app/controllers/Forgot_password1.php
@@ -0,0 +1,33 @@
+<?php
+
+class Forgot_password1 extends Controller
+{
+
+    public function index()
+    {
+        $user = new User;
+        $data['errors'] = [];
+        $data['title'] = "Forgot";
+
+        $data['rows']=[];
+        $rows = array();
+        $rows=$user->where(['id' => $_GET['data']]);
+        $data['rows']=$rows[0];
+
+        if($_SERVER['REQUEST_METHOD'] == "POST"){
+            if (empty($_POST['password'])) {
+                message("Password is required.");
+            } elseif (strlen($_POST['password']) < 8) {
+                message("Password must be at least 8 characters long.");
+            } elseif ($_POST['password'] == $_POST['repassword']) {
+                
+               $user->update($_GET['data'],$_POST);
+                redirect('Login/');
+            }else{
+                
+                message("Passwords do not match.");
+            }
+        }
+        $this->view('forgot_password1', $data);
+    }
+}
